0721834b2ca0d26e0e3f52df53a24d5de42d419e
Carlos Hernandez <carlos@hrndz.ca>
2023-06-24 18:34:18 -0400

    Try to use cachix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://hurricanehrndz.cachix.org"
+      "https://cache.nixos.org "
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hurricanehrndz.cachix.org-1:rKwB3P3FZ0T0Ck1KierCaO5PITp6njsQniYlXPVhFuA="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
     extra-experimental-features = "nix-command flakes";
   };
