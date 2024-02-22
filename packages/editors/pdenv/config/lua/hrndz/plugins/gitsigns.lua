bf72cb23e016a1bde2e169ae0b9a711cba37a9bd
Carlos Hernandez <carlos@hrndz.ca>
2024-02-22 15:42:55 -0700

    Update Gitsigns bindings

--- packages/editors/pdenv/config/lua/hrndz/plugins/gitsigns.lua
+++ packages/editors/pdenv/config/lua/hrndz/plugins/gitsigns.lua
@@ -4,33 +4,36 @@ local wk = require("which-key")
 gitsigns.setup({
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
-
-    local function map(mode, l, r, desc, opts)
-      opts = opts or {}
-      opts.desc = desc
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
+    local map = vim.keymap.set
 
     -- Navigation
-    map('n', ']h', gs.next_hunk, "Next Hunk")
-    map('n', '[h', gs.prev_hunk, "Prev Hunk")
+    map("n", "]c", function()
+      if vim.wo.diff then return "]c" end
+      vim.schedule(function() gs.next_hunk() end)
+      return "<Ignore>"
+    end, { expr = true, desc = "Next Change", buffer = bufnr })
+
+    map("n", "[c", function()
+      if vim.wo.diff then return "[c" end
+      vim.schedule(function() gs.prev_hunk() end)
+      return "<Ignore>"
+    end, { expr = true, desc = "Prev Change", buffer = bufnr })
 
     -- Actions
     wk.register({
       ["<leader>h"] = { name = "+hunks" },
-      ["<leader>hS"] = { gs.stage_buffer,  "Stage Buffer"},
-      ["<leader>hu"] = { gs.undo_stage_hunk, "Undo Stage Hunk"},
+      ["<leader>hS"] = { gs.stage_buffer, "Stage Buffer" },
+      ["<leader>hu"] = { gs.undo_stage_hunk, "Undo Stage Hunk" },
       ["<leader>hR"] = { gs.reset_buffer, "Reset Buffer" },
-      ['<leader>hp'] = { gs.preview_hunk, "Preview Hunk" },
+      ["<leader>hp"] = { gs.preview_hunk, "Preview Hunk" },
       ["<leader>hb"] = { function() gs.blame_line({ full = true }) end, "Blame Line" },
       ["<leader>hd"] = { gs.diffthis, "Diff This" },
-      ["<leader>hD"] = { function() gs.diffthis('~') end, "Diff This ~" },
-      ["<leader>hg"] = { gs.toggle_deleted, "View Deleted" }
-    })
-    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      ["<leader>hD"] = { function() gs.diffthis("~") end, "Diff This ~" },
+      ["<leader>hg"] = { gs.toggle_deleted, "View Deleted" },
+    }, { mode = "n", buffer = bufnr, silent = true, noremap =  true })
+    map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk", buffer = bufnr })
+    map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk", buffer = bufnr })
     -- Text object
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-  end
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns Select Hunk", buffer = bufnr })
+  end,
 })
