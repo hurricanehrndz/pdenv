708efbdec2f8104069fcbbac3da8214c39d5d53e
Carlos Hernandez <carlos@hrndz.ca>
2024-10-08 14:28:21 -0600

    feat: Keybind for buffer reload

--- packages/editors/pdenv/config/lua/hrndz/plugins/mini.lua
+++ packages/editors/pdenv/config/lua/hrndz/plugins/mini.lua
@@ -45,4 +45,5 @@ if vim.g.vscode == nil then
   map("n", "<leader>bD", function()
     require("mini.bufremove").delete(0, true)
   end, { desc = "Delete Buffer (Force)" })
+  map("n", "<leader>br", "<cmd>edit!<cr>", { desc = "Buffer Reload" })
 end
