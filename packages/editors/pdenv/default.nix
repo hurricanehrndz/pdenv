dd7d25c93f2eb76370968df3bfc66ee2a909b790
Carlos Hernandez <carlos@hrndz.ca>
2025-09-25 17:09:56 -0600

    Cache neovim builds

--- packages/editors/pdenv/default.nix
+++ packages/editors/pdenv/default.nix
@@ -10,6 +10,7 @@
   nvimPython = pkgs.python3.withPackages (ps: with ps; [debugpy flake8]);
   extraPackages = import ./extraPackages.nix {inherit pkgs packages nvimPython inputs system;};
   extraPackagesBinPath = "${lib.makeBinPath extraPackages}";
+
   nvimDict = pkgs.stdenv.mkDerivation rec {
     name = "nvimDict";
     dontUnpack = true;
@@ -19,52 +20,61 @@
       aspell -d en dump master | aspell -l en expand > $out/en.dict
     '';
   };
-  neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
-    withRuby = false;
-    withPython3 = false;
-    viAlias = true;
-    vimAlias = true;
-    plugins = import ./plugins.nix {inherit pkgs packages inputs nvimPython;};
-    wrapRc = false;
-  };
-  # add debugserver
-  # see: https://discourse.nixos.org/t/integrating-vadimcn-codelldb-with-neovim-on-nixos-python-module-error/56639/2
-  ext = inputs.nix-vscode-extensions.extensions.${system};
-  codelldb = ext.vscode-marketplace.vadimcn.vscode-lldb;
-  debugserverPath = "${codelldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
-  nvimrc = pkgs.stdenv.mkDerivation rec {
-    name = "nvimrc";
+
+  # This is your base neovim - only rebuilds when plugins change
+  baseNeovim = let
+    neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+      withRuby = false;
+      withPython3 = false;
+      viAlias = false;
+      vimAlias = false;
+      plugins = import ./plugins.nix {inherit pkgs packages inputs nvimPython;};
+      wrapRc = false;
+    };
+    wrapperArgs = lib.escapeShellArgs (neovimConfig.wrapperArgs
+      ++ [
+        "--suffix"
+        "PATH"
+        ":"
+        "${extraPackagesBinPath}"
+      ]);
+  in
+    pkgs.wrapNeovimUnstable inputs'.neovim-flake.packages.default (neovimConfig // {inherit wrapperArgs;});
+
+  # Separate config derivation
+  nvimConfig = pkgs.stdenv.mkDerivation rec {
+    name = "nvim-config";
     src = ./config;
     installPhase = ''
       mkdir -p $out/
       cp -r ./* $out/
       mkdir -p $out/lua/hrndz/generated
-      cp ${pkgs.writeText "$out/init.lua" ''
-        local M = {}
-        M.run = function ()
-          -- Global vars
-          vim.g.nix_dap_python = "${nvimPython}/bin/python"
-          vim.g.user_provided_dict = "${nvimDict}/en.dict"
-          vim.g.nix_dap_codelldb  = "${debugserverPath}"
-        end
 
-        return M
-      ''} $out/lua/hrndz/generated/init.lua
+      # Create generated config
+      cat > $out/lua/hrndz/generated/init.lua << 'EOF'
+      local M = {}
+      M.run = function ()
+        -- Global vars
+        vim.g.nix_dap_python = "${nvimPython}/bin/python"
+        vim.g.user_provided_dict = "${nvimDict}/en.dict"
+        vim.g.nix_dap_codelldb  = "${debugserverPath}"
+      end
+      return M
+      EOF
     '';
   };
-  # convert to string to stop doublbing of args
-  wrapperArgs = lib.escapeShellArgs (neovimConfig.wrapperArgs
-    ++ [
-      "--suffix"
-      "PATH"
-      ":"
-      "${extraPackagesBinPath}"
-      "--add-flags"
-      ''--cmd "set rtp^=${nvimrc}"''
-      "--add-flags"
-      ''-u "${nvimrc}/init.lua"''
-    ]);
-  LuaConfig = neovimConfig // {inherit wrapperArgs;};
+
+  ext = inputs.nix-vscode-extensions.extensions.${system};
+  codelldb = ext.vscode-marketplace.vadimcn.vscode-lldb;
+  debugserverPath = "${codelldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+  nvimCmd = "${baseNeovim}/bin/nvim --cmd \"set rtp^=${nvimConfig}\" -u \"${nvimConfig}/init.lua\"";
 in
-  # wrap my neovim pkg override
-  pkgs.wrapNeovimUnstable inputs'.neovim-flake.packages.default LuaConfig
+  pkgs.symlinkJoin {
+    name = "nvim";
+    paths = [
+      (pkgs.writeShellScriptBin "nvim" ''exec ${nvimCmd} "$@"'')
+      (pkgs.writeShellScriptBin "vi" ''exec ${nvimCmd} "$@"'')
+      (pkgs.writeShellScriptBin "vim" ''exec ${nvimCmd} "$@"'')
+      (pkgs.writeShellScriptBin "nvim-minimal" ''exec ${baseNeovim}/bin/nvim --clean "$@"'')
+    ];
+  }
