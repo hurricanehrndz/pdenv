ffc220fdfd4a676016a51d1e6f22689c9a6c7b13
Carlos Hernandez <carlos@hrndz.ca>
2025-09-28 00:18:42 -0600

    feat(snacks): add git maps

--- packages/editors/pdenv/config/lua/hrndz/plugins/snacks.lua
+++ packages/editors/pdenv/config/lua/hrndz/plugins/snacks.lua
@@ -104,14 +104,17 @@ Snacks.toggle
   .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
   :map("<leader>uc")
 Snacks.toggle.treesitter():map("<leader>uT")
+-- cause an error when deleted -- check lazyvim distro for treesitter settings
 Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
 Snacks.toggle.inlay_hints():map("<leader>uh")
 
 -- git
-map("n", "<leader>gb", function() Snacks.picker.git_branches() end, { desc = "Git Branches" })
 map("n", "<leader>gl", function() Snacks.picker.git_log() end, { desc = "Git Log" })
-map("n", "<leader>gL", function() Snacks.picker.git_log_line() end, { desc = "Git Log Line" })
-map("n", "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" })
-map("n", "<leader>gS", function() Snacks.picker.git_stash() end, { desc = "Git Stash" })
--- map("n", "<leader>gd", function() Snacks.picker.git_diff() end, { desc = "Git Diff (Hunks)" })
 map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Log File" })
+map("n", "<leader>gb", function() Snacks.picker.git_log_line() end, { desc = "Git Blame Line" })
+map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
+map("n", "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" })
+map({ "n", "x" }, "<leader>gB", function() Snacks.gitbrowse() end, { desc = "Git Browse (open)" })
+map({ "n", "x" }, "<leader>gY", function()
+  Snacks.gitbrowse({ open = function(url) vim.fn.setreg("+", url) end, notify = false })
+end, { desc = "Git Browse (copy)" })
