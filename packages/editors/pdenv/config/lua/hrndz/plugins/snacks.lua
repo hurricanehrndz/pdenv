0d5be1e79fb966590066fc4ae19da34b4340247d
Carlos Hernandez <carlos@hrndz.ca>
2025-09-29 10:29:53 -0600

    feat(snacks): fix git mappings

--- packages/editors/pdenv/config/lua/hrndz/plugins/snacks.lua
+++ packages/editors/pdenv/config/lua/hrndz/plugins/snacks.lua
@@ -183,12 +183,10 @@ Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Ba
 Snacks.toggle.inlay_hints():map("<leader>uh")
 
 -- git
+map("n", "<leader>gb", function() Snacks.picker.git_branches() end, { desc = "Git Branches" })
 map("n", "<leader>gl", function() Snacks.picker.git_log() end, { desc = "Git Log" })
-map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Log File" })
-map("n", "<leader>gb", function() Snacks.picker.git_log_line() end, { desc = "Git Blame Line" })
-map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
+map("n", "<leader>gL", function() Snacks.picker.git_log_line() end, { desc = "Git Log Line" })
 map("n", "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" })
-map({ "n", "x" }, "<leader>gB", function() Snacks.gitbrowse() end, { desc = "Git Browse (open)" })
-map({ "n", "x" }, "<leader>gY", function()
-  Snacks.gitbrowse({ open = function(url) vim.fn.setreg("+", url) end, notify = false })
-end, { desc = "Git Browse (copy)" })
+map("n", "<leader>gS", function() Snacks.picker.git_stash() end, { desc = "Git Stash" })
+-- map("n", "<leader>gd", function() Snacks.picker.git_diff() end, { desc = "Git Diff (Hunks)" })
+map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Log File" })
