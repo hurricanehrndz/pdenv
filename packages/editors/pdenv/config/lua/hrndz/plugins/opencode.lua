f375885fa56816e6cfd459b3fd137440558c28de
Carlos Hernandez <carlos@hrndz.ca>
2025-10-24 10:29:07 -0600

    Toggle normal mode in opencode via <esc><esc>

--- packages/editors/pdenv/config/lua/hrndz/plugins/opencode.lua
+++ packages/editors/pdenv/config/lua/hrndz/plugins/opencode.lua
@@ -9,3 +9,27 @@ vim.keymap.set("n", "<leader>oi", function() require("opencode").command("sessio
 vim.keymap.set("n", "<leader>oA", function() require("opencode").command("agent_cycle") end, { desc = "Cycle selected agent" })
 vim.keymap.set("n", "<S-C-u>",    function() require("opencode").command("messages_half_page_up") end, { desc = "Messages half page up" })
 vim.keymap.set("n", "<S-C-d>",    function() require("opencode").command("messages_half_page_down") end, { desc = "Messages half page down" })
+
+local esc_timer = nil
+
+vim.api.nvim_create_augroup("OpenCodeTerminal", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = "OpenCodeTerminal",
+	callback = function()
+		local bufname = vim.api.nvim_buf_get_name(0)
+		if bufname:match("opencode$") then
+      vim.keymap.set("t", "<esc>",
+      function()
+        esc_timer = esc_timer or (vim.uv or vim.loop).new_timer()
+        if esc_timer:is_active() then
+          esc_timer:stop()
+          vim.cmd("stopinsert")
+        else
+          esc_timer:start(200, 0, function() end)
+          return "<esc>"
+        end
+      end,
+      { desc = "Double escape to normal mode", buffer = 0, expr = true })
+		end
+	end,
+})
