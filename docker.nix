48d5d3981fe8ee681a6fe8c8bc54279154305c09
Carlos Hernandez <carlos@hrndz.ca>
2026-01-21 13:30:15 -0700

    ci: update release workflow and add docker build config
    - Update GitHub Actions versions (checkout v4, DeterminateSystems nix-installer, cachix v15, docker/login v3)
    - Switch cachix job to multi-system matrix (x86_64-linux, aarch64-linux, aarch64-darwin)
    - Simplify build to use cachix pin instead of watch-exec
    - Add latest tag to docker manifest builds
    - Disable workflow_dispatch and scheduled runs
    - Add standalone docker.nix for building OCI images

---
+++ docker.nix
@@ -0,0 +1,50 @@
+{
+  name,
+  tag,
+  rev ? null,
+  system ? builtins.currentSystem,
+  arch,
+}: let
+  flake = builtins.getFlake ("git+file:${toString ./.}"
+    + (
+      if !(isNull rev)
+      then "?rev=${rev}&shallow=1"
+      else ""
+    ));
+  inherit (flake.packages.${system}) pdenv neovim;
+  nixpkgs = system: import flake.inputs.nixpkgs {inherit system;};
+  mkDocker = {
+    pkgs,
+    name ? "ghcr.io/hurricanehrndz/pdenv",
+    tag ? null,
+    arch,
+  }:
+    pkgs.dockerTools.buildImage {
+      inherit name;
+      tag =
+        if isNull tag
+        then neovim.version
+        else tag;
+      architecture =
+        if arch == "arm64"
+        then "arm64"
+        else "amd64";
+      copyToRoot = pkgs.buildEnv {
+        name = "pdenv";
+        paths = [
+          pdenv
+        ];
+      };
+      config = {
+        Entrypoint = ["/bin/nvim"];
+      };
+      # layers = [
+      #   (n2c.buildLayer {deps = [pdenv];})
+      # ];
+    };
+in
+  mkDocker {
+    pkgs = nixpkgs system;
+    inherit name tag arch;
+  }
+
