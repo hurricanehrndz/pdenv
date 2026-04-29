8f9450080348e37ddcd32f6ff16b70b426a3d417
Carlos Hernandez <carlos@hrndz.ca>
2026-04-29 07:31:05 -0600

    Fix neotest run file mapping

--- per-system/pkgs/by-name/pdenv/config/lua/hrndz/plugins/neotest.lua
+++ per-system/pkgs/by-name/pdenv/config/lua/hrndz/plugins/neotest.lua
@@ -19,7 +19,7 @@ require("neotest").setup({
 })
 
 local map = vim.keymap.set
-map("n", "<leaer>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "Run File" })
+map("n", "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "Run File" })
 map("n", "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, { desc = "Run All Test Files" })
 map("n", "<leader>tr", function() require("neotest").run.run() end, { desc = "Run Nearest" })
 map("n", "<leader>ts", function() require("neotest").summary.toggle() end, { desc = "Toggle Summary" })
