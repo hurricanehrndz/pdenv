5e3d45bcdb1dac9790109bcfa2ed6e59fe15b19b
Carlos Hernandez <carlos@hrndz.ca>
2026-04-29 08:11:00 -0600

    Use Snacks picker for todos

--- per-system/pkgs/by-name/pdenv/config/lua/hrndz/plugins/todo.lua
+++ per-system/pkgs/by-name/pdenv/config/lua/hrndz/plugins/todo.lua
@@ -3,5 +3,12 @@ require("todo-comments").setup({})
 local map = vim.keymap.set
 map("n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" })
 map("n", "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" })
+map("n", "<leader>st", function() Snacks.picker.pick("todo_comments") end, { desc = "Todo" })
+map(
+  "n",
+  "<leader>sT",
+  function() Snacks.picker.pick("todo_comments", { keywords = { "TODO", "FIX", "FIXME" } }) end,
+  { desc = "Todo/Fix/Fixme" }
+)
 map("n", "<leader>xt", "<cmd>TodoTrouble<cr>", { desc = "Todo (Trouble)" })
 map("n", "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", { desc = "Todo/Fix/Fixme (Trouble)" })
