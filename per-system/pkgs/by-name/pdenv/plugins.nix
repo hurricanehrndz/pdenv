a3ff9259b99163cec66bb314ed5fd864585d6d0f
Carlos Hernandez <chernand@yelp.com>
2026-06-22 21:35:22 -0600

    fix(plugins): wire neovim plugin check deps

--- per-system/pkgs/by-name/pdenv/plugins.nix
+++ per-system/pkgs/by-name/pdenv/plugins.nix
@@ -16,25 +16,108 @@ let
   blink-cmp = pkgs.blinkcmpFlake.blink-cmp.overrideAttrs (_: {
     doCheck = false;
   });
+  lazy-nvim = pkgs.vimExtraPlugins.lazy-nvim-folke.overrideAttrs (_: {
+    nvimRequireCheck = [
+      "lazy.docs"
+      "lazy.util"
+    ];
+  });
+  snacks-nvim = pkgs.vimExtraPlugins.snacks-nvim-folke.overrideAttrs (_: {
+    # Optional Trouble profiler bridge; add Trouble here if the profiler integration is used.
+    nvimSkipModules = [ "trouble.sources.profiler" ];
+  });
+  tokyonight-nvim = pkgs.vimExtraPlugins.tokyonight-nvim-folke.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ lazy-nvim ];
+  });
+  catppuccin-nvim = pkgs.vimExtraPlugins.catppuccin-catppuccin.overrideAttrs (_: {
+    # Integration module assumes catppuccin setup state exists at require time.
+    nvimSkipModules = [ "catppuccin.groups.integrations.noice" ];
+  });
+  noice-nvim = pkgs.vimExtraPlugins.noice-nvim-folke.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [
+      pkgs.vimExtraPlugins.nui-nvim-MunifTanjim
+      snacks-nvim
+      pkgs.vimExtraPlugins.mini-nvim-nvim-mini
+    ];
+  });
+  which-key-nvim = pkgs.vimExtraPlugins.which-key-nvim-folke.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ lazy-nvim ];
+  });
+  trouble-nvim = pkgs.vimExtraPlugins.trouble-nvim-folke.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ lazy-nvim ];
+    # Docs generator executes at require time and needs initialized Trouble config.
+    nvimSkipModules = [ "trouble.docs" ];
+  });
+  todo-comments-nvim = pkgs.vimExtraPlugins.todo-comments-nvim-folke.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ trouble-nvim ];
+    # FZF integration is optional in this config; add fzf-lua here if enabling it.
+    nvimSkipModules = [ "todo-comments.fzf" ];
+  });
+  tiny-code-action-nvim = pkgs.vimExtraPlugins.tiny-code-action-nvim-rachartier.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ snacks-nvim ];
+  });
+  plenary-nvim = pkgs.vimExtraPlugins.plenary-nvim-nvim-lua.overrideAttrs (_: {
+    # Optional neorocks helper is not needed by this config.
+    nvimSkipModules = [ "plenary.neorocks.init" ];
+  });
+  nvim-nio = pkgs.vimExtraPlugins.nvim-nio-nvim-neotest;
+  nvim-dap = pkgs.vimExtraPlugins.nvim-dap-mfussenegger;
+  nvim-dap-ui = pkgs.vimExtraPlugins.nvim-dap-ui-rcarriga.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [
+      nvim-dap
+      nvim-nio
+    ];
+  });
+  nvim-dap-virtual-text = pkgs.vimExtraPlugins.nvim-dap-virtual-text-theHamsta.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ nvim-dap ];
+  });
+  nvim-dap-python = pkgs.vimExtraPlugins.nvim-dap-python-mfussenegger.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ nvim-dap ];
+  });
+  nvim-dap-go = pkgs.vimExtraPlugins.nvim-dap-go-leoluz.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ nvim-dap ];
+  });
+  neotest-nvim = pkgs.vimExtraPlugins.neotest-nvim-neotest.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [
+      plenary-nvim
+      nvim-nio
+    ];
+  });
+  neotest-golang = pkgs.vimExtraPlugins.neotest-golang-fredrikaverpil.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [
+      neotest-nvim
+      plenary-nvim
+      nvim-nio
+    ];
+  });
+  zk-nvim = pkgs.vimExtraPlugins.zk-nvim-zk-org.overrideAttrs (old: {
+    dependencies = (old.dependencies or [ ]) ++ [ snacks-nvim ];
+    # Config uses snacks_picker; other picker integrations are intentionally not installed.
+    nvimSkipModules = [
+      "zk.pickers.minipick"
+      "zk.pickers.telescope"
+      "zk.pickers.fzf_lua"
+    ];
+  });
 in
 with pkgs.vimExtraPlugins;
 [
   # Theme
   rose-pine-rose-pine
-  tokyonight-nvim-folke
-  catppuccin-catppuccin
+  tokyonight-nvim
+  catppuccin-nvim
   gitsigns-nvim-lewis6991
   nvim-colorizer-catgoose-catgoose
   nvim-window-yorickpeterse
   fidget-nvim-j-hui
   lualine-nvim-nvim-lualine
-  todo-comments-nvim-folke
+  todo-comments-nvim
   # folke stuff
   lazydev-nvim-folke
-  snacks-nvim-folke
+  snacks-nvim
   # required for noice
   nui-nvim-MunifTanjim
-  noice-nvim-folke
+  noice-nvim
 
   # add some syntax highlighting
   (pkgs.vimPlugins.nvim-treesitter.withPlugins (
@@ -62,9 +145,9 @@ with pkgs.vimExtraPlugins;
   ))
 
   # which key did I just hit
-  which-key-nvim-folke
+  which-key-nvim
   # what's did I do wrong
-  trouble-nvim-folke
+  trouble-nvim
   diffview-nvim-sindrets
 
   # distributions
@@ -86,7 +169,7 @@ with pkgs.vimExtraPlugins;
   nvim-lsp-endhints-chrisgrieser
 
   # CodeActions preview
-  tiny-code-action-nvim-rachartier
+  tiny-code-action-nvim
 
   # nice plugins
   nvim-osc52-ojroques
@@ -102,24 +185,24 @@ with pkgs.vimExtraPlugins;
   pkgs.vimPlugins.comment-nvim
 
   # debugging
-  nvim-dap-mfussenegger
-  nvim-dap-ui-rcarriga
-  nvim-dap-virtual-text-theHamsta
-  nvim-dap-python-mfussenegger
-  nvim-dap-go-leoluz
+  nvim-dap
+  nvim-dap-ui
+  nvim-dap-virtual-text
+  nvim-dap-python
+  nvim-dap-go
 
   # filetype
   pkgs.vimPlugins.Jenkinsfile-vim-syntax
   pkgs.vimPlugins.vim-puppet
 
   # neotest testing
-  nvim-nio-nvim-neotest
-  neotest-nvim-neotest
-  neotest-golang-fredrikaverpil
+  nvim-nio
+  neotest-nvim
+  neotest-golang
   pkgs.vimPlugins.FixCursorHold-nvim
 
   # async - used by many plugins
-  plenary-nvim-nvim-lua
+  plenary-nvim
 
   # copy reference
   copy-reference-nvim
@@ -128,5 +211,5 @@ with pkgs.vimExtraPlugins;
   wezterm-types-DrKJeff16
 
   # notes
-  zk-nvim-zk-org
+  zk-nvim
 ]
